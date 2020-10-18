14392f78b1e2a7abd1dc15881d148ca35e71ad3a
ErkebulanMuhamedkali <160107002@stu.sdu.edu.kz>
2020-10-18 13:45:11 +0600

    stated scraping first page

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello")
+import requests
+from bs4 import BeautifulSoup
+
+host = "https://avtoelon.uz/"
+category = "avto/"
+
+response = requests.get(host + category)
+
+html = response.text
+
+soup = BeautifulSoup(html, "html.parser")
+
+types = ["", " red", " blue", " yellow"]
+
+for t in types:
+    ads = soup.find_all("div", class_=f"row list-item{t} a-elem")
+    ids = [ad["data-id"] for ad in ads]
+    print(ids)
