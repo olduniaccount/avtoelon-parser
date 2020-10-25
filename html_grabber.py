7fc17ac7fc10d4227bb789a90141d0927b236b05
ErkebulanMuhamedkali <160107002@stu.sdu.edu.kz>
2020-10-25 13:34:47 +0600

    pandas csv save

---
+++ html_grabber.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def grab(url: str) -> str:
+    return requests.get(url).text
+
+
+def get_ids(url: str) -> list:
+    soup = BeautifulSoup(grab(url), "html.parser")
+
+    types = ["", " red", " blue", " yellow"]
+
+    pre_result = list()
+
+    for t in types:
+        ads = soup.find_all("div", class_=f"row list-item{t} a-elem")
+        ids = [ad["data-id"] for ad in ads]
+        pre_result.append(ids)
+
+    return [j for i in pre_result for j in i]
+
+
+def get_max_page(url: str) -> int:
+    soup = BeautifulSoup(grab(url), "html.parser")
+    return int(soup.find(class_="pager").find('ul').find_all('li')[-1].text)
--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
+from html_grabber import get_ids, get_max_page
+
+
+def save_data_to_csv(id_list: list):
+    import pandas as pd
+    df = pd.DataFrame(id_list, columns=["colummn"])
+    df.to_csv('list.csv', index=False)
+
+
 host = "https://avtoelon.uz/"
 category = "avto/"
 
-response = requests.get(host + category)
+page_num = 1
+ids_count = 0
+while page_num <= get_max_page(host + category):
+    ids = get_ids(host + category + "?page=" + str(page_num))
+    ids_count += len(ids)
+    print("page =", page_num, "ids found =", len(ids))
+    save_data_to_csv(id_list=ids)
+    page_num += 1
+    if page_num == 3:
+        break
 
-html = response.text
-
-soup = BeautifulSoup(html, "html.parser")
-
-types = ["", " red", " blue", " yellow"]
-
-for t in types:
-    ads = soup.find_all("div", class_=f"row list-item{t} a-elem")
-    ids = [ad["data-id"] for ad in ads]
-    print(ids)
+print()
+print(ids_count)
