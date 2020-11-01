3ae98462bb6bdc5f6828fd77af1820606ac35e06
ErkebulanMuhamedkali <160107002@stu.sdu.edu.kz>
2020-11-01 13:31:32 +0600

    details parse, and save to csv

---
+++ details.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+
+from html_grabber import grab
+
+
+def get_details(ad_id: str, host: str):
+    soup = BeautifulSoup(grab(f"{host}a/show/{ad_id}"), "html.parser")
+
+    brand = soup.find('span', {"itemprop": "brand"}).text.strip()
+    name = soup.find('h1', class_='a-title__text').find('span', {"itemprop": "name"}).text.strip()
+    year = int(soup.find('span', class_='year').text.strip())
+    price = int(''.join(soup.find('span', class_='a-price__text').text.strip().split('\xa0')[:-1]).replace('~', ''))
+    pre_result = dict(brand=brand, name=name, year=year, price=price)
+
+    description = soup.find('dl', class_='clearfix dl-horizontal description-params')
+    keys = [k.text.strip() for k in description.find_all('dt', class_='value-title')]
+    values = [v.text.strip() for v in description.find_all('dd', class_='value clearfix')]
+
+    parameters = dict(zip(keys, values))
+    if 'Пробег' in parameters.keys():
+        parameters['Пробег'] = int(''.join(parameters.get('Пробег').split(' ')[:-1]))
+
+    result = {'id': ad_id, **pre_result, **parameters}
+
+    views = soup.find('div', class_='col-sm-4 nb-views').text.strip()
+    pub_date = soup.find('div', class_='col-sm-4').text.strip()
+
+    print(f"{ad_id} done")
+    return result
--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-import requests
-from bs4 import BeautifulSoup
-
+from details import get_details
 from html_grabber import get_ids, get_max_page
 
 
-def save_data_to_csv(id_list: list):
+def save_data_to_csv(adverts: list):
     import pandas as pd
-    df = pd.DataFrame(id_list, columns=["colummn"])
+    df = pd.DataFrame(adverts)
     df.to_csv('list.csv', index=False)
 
 
 host = "https://avtoelon.uz/"
 category = "avto/"
 
+all_data = []
+
 page_num = 1
 ids_count = 0
 while page_num <= get_max_page(host + category):
     ids = get_ids(host + category + "?page=" + str(page_num))
+    details = [get_details(i, host) for i in ids]
     ids_count += len(ids)
     print("page =", page_num, "ids found =", len(ids))
-    save_data_to_csv(id_list=ids)
+    all_data.append(details)
     page_num += 1
-    if page_num == 3:
-        break
+
+save_data_to_csv([j for i in all_data for j in i])
 
 print()
 print(ids_count)
